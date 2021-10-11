40bea8c5ed9347051fbe4ca82baca1cf4eb50bbf
kostrom3n <dmitriianisiforov13@gmail.com>
2021-10-11 11:30:51 -0500

    added main method to a third file\package

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,5 +4,10 @@ public class Third {
     public void w(){
         System.out.println("sdalpdslapldpasldplasd");
 
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("4");
     }
 }
