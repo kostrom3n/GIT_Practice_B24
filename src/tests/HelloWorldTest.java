06ea6558a91220f8a1ce0bf1f54c940e570fa493
kostrom3n <dmitriianisiforov13@gmail.com>
2021-10-11 16:01:49 -0500

    Added  one more hello world

--- src/tests/HelloWorldTest.java
+++ src/tests/HelloWorldTest.java
@@ -5,5 +5,7 @@ public class HelloWorldTest {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+
+        System.out.println("One more Hello World");
     }
 }
